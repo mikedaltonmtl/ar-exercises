bc98846913a20ed15e91b63939e94774fe7bc939
mikedaltonmtl <michael.j.dalton@gmail.com>
2023-03-06 23:47:04 +0000

    complete exercise_4.rb

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -6,4 +6,45 @@ require_relative './exercise_3'
 puts "Exercise 4"
 puts "----------"
 
-# Your code goes here ...
+=begin
+Exercise 4: Loading a subset of stores
+Borrowing and modifying the code from Exercise one, create 3 more stores:
+Surrey (annual_revenue of 224000, carries women's apparel only)
+Whistler (annual_revenue of 1900000 carries men's apparel only)
+Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+Using the where class method from Active Record, fetch (a collection of) only those stores that carry men's apparel.
+Assign the results to a variable @mens_stores.
+Loop through each of these stores and output their name and annual revenue on each line.
+Do another fetch but this time load stores that carry women's apparel and are generating
+less than $1M in annual revenue.
+=end
+
+surrey = Store.create(
+  name: "Surrey",
+  annual_revenue: 224_000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+whistler = Store.create(
+  name: "Whistler",
+  annual_revenue: 1_900_000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+yaletown = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430_000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each { |store| puts "#{store.name} #{store.annual_revenue}" }
+
+@womens_small_stores = Store
+  .where(womens_apparel: true)
+  .where("annual_revenue < 1000000")
+
+@womens_small_stores.each { |store| puts "#{store.name} #{store.annual_revenue}" }
